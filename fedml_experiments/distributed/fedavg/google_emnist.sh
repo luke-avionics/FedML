fe3a4344a315ee56344f4042942e3fe2bc378f56
LukeZhang <sirsargras@gmail.com>
2020-11-13 06:19:32 -0600

    run script

--- fedml_experiments/distributed/fedavg/google_emnist.sh
+++ fedml_experiments/distributed/fedavg/google_emnist.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-CLIENT_NUM=8
-WORKER_NUM=8
+CLIENT_NUM=3400
+WORKER_NUM=10
 SERVER_NUM=1
-GPU_NUM_PER_SERVER=4
-MODEL='resnet38'
+GPU_NUM_PER_SERVER=8
+MODEL='cnn'
 DISTRIBUTION='hetero'
 ROUND=2000
 EPOCH=10
-BATCH_SIZE=32
-LR='0.01'
+BATCH_SIZE=20
+LR='0.1'
 INFERENCE_BITS=0
 CYCLIC_NUM_BITS_SCHEDULE='None'
 LR_DECAY_STEP_SIZE=2000
-DATASET='cifar100'
-DATA_DIR="/home/yz87/FedML/fedml_experiments/distributed/fedavg"
+DATASET='femnist'
+DATA_DIR="../../../data/FederatedEMNIST"
 
 PROCESS_NUM=`expr $WORKER_NUM + 1`
 echo $PROCESS_NUM
@@ -37,4 +37,3 @@ mpirun -np $PROCESS_NUM -hostfile ./mpi_host_file python3 ./main_fedavg.py \
   --inference_bits $INFERENCE_BITS \
   --cyclic_num_bits_schedule $CYCLIC_NUM_BITS_SCHEDULE \
   --lr_decay_step_size $LR_DECAY_STEP_SIZE
-
