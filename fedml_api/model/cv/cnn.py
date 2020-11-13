56f60566ac1b1f5145afdb7148a275b40bd928e1
LukeZhang <sirsargras@gmail.com>
2020-11-13 00:04:14 -0600

    emnist quant

--- fedml_api/model/cv/cnn.py
+++ fedml_api/model/cv/cnn.py
@@ -1,6 +1,47 @@
 import torch
 import torch.nn as nn
+from .quantize import QConv2d, RangeBN
+from .quantize_rl import  QLinear
 
+
+ACT_FW = 0
+ACT_BW = 0
+GRAD_ACT_ERROR = 0
+GRAD_ACT_GC = 0
+
+MOMENTUM = 0.9
+
+DWS_BITS = 8
+DWS_GRAD_BITS = 16
+
+def Conv3x3(in_planes, out_planes, stride=1):
+    "3x3 convolution with padding"
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
+
+
+def conv3x3(in_planes, out_planes, stride=1):
+    "3x3 convolution with padding"
+    return QConv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                   padding=1, bias=True, momentum=MOMENTUM, quant_act_forward=ACT_FW, quant_act_backward=ACT_BW,
+                   quant_grad_act_error=GRAD_ACT_ERROR, quant_grad_act_gc=GRAD_ACT_GC)
+
+
+def conv(in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1, groups=1, bias=True):
+    return QConv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride,
+                   padding=padding, dilation=dilation, groups=groups, bias=bias, momentum=MOMENTUM, quant_act_forward=ACT_FW, quant_act_backward=ACT_BW,
+                   quant_grad_act_error=GRAD_ACT_ERROR, quant_grad_act_gc=GRAD_ACT_GC)
+def Linear(in_features, out_features, bias=True, dropout=0):
+    """Linear layer (input: N x T x C)"""
+    m = QLinear(in_features, out_features, bias=bias)
+    m.weight.data.uniform_(-0.1, 0.1)
+    if bias:
+        m.bias.data.uniform_(-0.1, 0.1)
+    return m
+
+def make_bn(planes):
+	return nn.BatchNorm2d(planes)
+	# return RangeBN(planes)
 
 class CNN_OriginalFedAvg(torch.nn.Module):
     """The CNN model used in the original FedAvg paper:
@@ -47,24 +88,28 @@ class CNN_OriginalFedAvg(torch.nn.Module):
     def __init__(self, only_digits=True):
         super(CNN_OriginalFedAvg, self).__init__()
         self.only_digits = only_digits
-        self.conv2d_1 = torch.nn.Conv2d(1, 32, kernel_size=5, padding=2)
+        self.conv2d_1 = conv(1, 32, kernel_size=5, padding=2)
+        #self.conv2d_1 = torch.nn.Conv2d(1, 32, kernel_size=5, padding=2)
         self.max_pooling = nn.MaxPool2d(2, stride=2)
-        self.conv2d_2 = torch.nn.Conv2d(32, 64, kernel_size=5, padding=2)
+        self.conv2d_2 = conv(32, 64, kernel_size=5, padding=2)
+        #self.conv2d_2 = torch.nn.Conv2d(32, 64, kernel_size=5, padding=2)
         self.flatten = nn.Flatten()
-        self.linear_1 = nn.Linear(3136, 512)
-        self.linear_2 = nn.Linear(512, 10 if only_digits else 62)
+        self.linear_1 = Linear(3136, 512)
+        #self.linear_1 = nn.Linear(3136, 512)
+        self.linear_2 = Linear(512, 10 if only_digits else 62)
+        #self.linear_2 = nn.Linear(512, 10 if only_digits else 62)
         self.relu = nn.ReLU()
         self.softmax = nn.Softmax(dim=1)
 
-    def forward(self, x):
+    def forward(self, x, num_bits=0):
         x = torch.unsqueeze(x, 1)
-        x = self.conv2d_1(x)
+        x = self.conv2d_1(x, num_bits)
         x = self.max_pooling(x)
-        x = self.conv2d_2(x)
+        x = self.conv2d_2(x, num_bits)
         x = self.max_pooling(x)
         x = self.flatten(x)
-        x = self.relu(self.linear_1(x))
-        x = self.softmax(self.linear_2(x))
+        x = self.relu(self.linear_1(x, num_bits))
+        x = self.softmax(self.linear_2(x, num_bits))
         return x
 
 
@@ -111,25 +156,30 @@ class CNN_DropOut(torch.nn.Module):
 
     def __init__(self, only_digits=True):
         super(CNN_DropOut, self).__init__()
-        self.conv2d_1 = torch.nn.Conv2d(1, 32, kernel_size=3)
+        self.conv2d_1 = conv(1, 32, kernel_size=3)
+        #self.conv2d_1 = torch.nn.Conv2d(1, 32, kernel_size=3)
         self.max_pooling = nn.MaxPool2d(2, stride=2)
-        self.conv2d_2 = torch.nn.Conv2d(32, 64, kernel_size=3)
+        self.conv2d_2 = conv(32, 64, kernel_size=3)
+        #self.conv2d_2 = torch.nn.Conv2d(32, 64, kernel_size=3)
         self.dropout_1 = nn.Dropout(0.25)
         self.flatten = nn.Flatten()
-        self.linear_1 = nn.Linear(9216, 128)
+        self.linear_1 = Linear(9216, 128)
+        #self.linear_1 = nn.Linear(9216, 128)
         self.dropout_2 = nn.Dropout(0.5)
-        self.linear_2 = nn.Linear(128, 10 if only_digits else 62)
+        self.linear_2 = Linear(128, 10 if only_digits else 62)
+        #self.linear_2 = nn.Linear(128, 10 if only_digits else 62)
         self.relu = nn.ReLU()
         self.softmax = nn.Softmax(dim=1)
 
-    def forward(self, x):
+    def forward(self, x, num_bits=0):
         x = torch.unsqueeze(x, 1)
-        x = self.conv2d_1(x)
-        x = self.conv2d_2(x)
+        x = self.conv2d_1(x, num_bits)
+        x = self.conv2d_2(x, num_bits)
+        x = self.relu(x)
         x = self.max_pooling(x)
         x = self.dropout_1(x)
         x = self.flatten(x)
-        x = self.relu(self.linear_1(x))
+        x = self.linear_1(x, num_bits)
         x = self.dropout_2(x)
-        x = self.softmax(self.linear_2(x))
+        x = self.softmax(self.linear_2(x, num_bits))
         return x
